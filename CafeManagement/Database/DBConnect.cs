729de1766964f5a300e481fff37f6c1878a956ce
Ninh Hai Nam <ninhhainam12@gmail.com>
2025-04-18 11:27:23 +0700

    first commit

---
+++ CafeManagement/Database/DBConnect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CafeManagement.Database
+{
+    internal class DBConnect
+    {
+        private static string connString = "Data Source=DESKTOP-RGO0IJA;Initial Catalog=cafe_mana;Integrated Security=True;Encrypt=False";
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ CafeManagement/Forms/Form1.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+namespace CafeManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SqlConnection conn =  Database.DBConnect.GetConnection();
+            if(conn != null)
+            {
+                MessageBox.Show("Kết nối thành công"); return;
+            } 
+        }
+    }
+}
