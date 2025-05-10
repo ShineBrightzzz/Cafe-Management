275d43f496841ea26c9b58836221274740b3ea9a
Ninh Hai Nam <ninhhainam12@gmail.com>
2025-05-10 10:22:37 +0700

    Interface service

---
+++ CafeManagement/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManagement.Services
+{
+    internal interface IService<T>
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+        T GetById(Guid id);
+        List<T> GetAll();
+        List<T> Search(string keyword);
+    }
+}
